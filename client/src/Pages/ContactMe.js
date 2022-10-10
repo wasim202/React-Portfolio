6904aecc6bab866738f9bc7c5b13eafb3c57a8f5
wasim202 <65715473+wasim202@users.noreply.github.com>
2022-10-10 17:05:43 -0600

    emailjs

--- client/src/Pages/ContactMe.js
+++ client/src/Pages/ContactMe.js
@@ -1,26 +1,50 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import emailjs from '@emailjs/browser';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 export default function ContactMe() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_rjf3qb9', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+//export default function ContactMe() {
   return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
      
-  <Form>
-     <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-      <Form.Label>Name</Form.Label>
-      <Form.Control type="input" placeholder="Ex: John Smith" />
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
-      <Form.Label>Email address</Form.Label>
-      <Form.Control type="email" placeholder="name@example.com" />
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-      <Form.Label>Message</Form.Label>
-      <Form.Control as="textarea" rows={3} />
-    </Form.Group>
-    <Button variant="primary" type="submit">
-        Submit
-      </Button>
-  </Form>
+  // <Form>
+  //    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+  //     <Form.Label>Name</Form.Label>
+  //     <Form.Control type="input" placeholder="Ex: John Smith" />
+  //   </Form.Group>
+  //   <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+  //     <Form.Label>Email address</Form.Label>
+  //     <Form.Control type="email" placeholder="name@example.com" />
+  //   </Form.Group>
+  //   <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+  //     <Form.Label>Message</Form.Label>
+  //     <Form.Control as="textarea" rows={3} />
+  //   </Form.Group>
+  //   <Button variant="primary" type="submit">
+  //       Submit
+  //     </Button>
+  // </Form>
   )
 }
--- client/src/Pages/Portfolio/Portfolio.js
+++ client/src/Pages/Portfolio/Portfolio.js
@@ -1,7 +1,47 @@
-import React from 'react'
+import React from 'react';
+import {Card, Container} from 'react-bootstrap';
+import FiveGuys from './Five-Guys.png';
+import MovieDB from './Movie-Database.png';
+import HomePage from './home-page.png';
+import ViewTables from './view-tables.png'
 
 export default function Portfolio() {
   return (
-    <div>Portfolio</div>
+    <>
+    <Container>
+      <Card sm ="6">
+        <Card.Img style={{}} variant="top" src={FiveGuys} />
+        <Card.Body>
+          <Card.Text>
+           <a href="https://arcane-mesa-45752.herokuapp.com/">Welcome to Five Guys</a>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src={MovieDB} />
+        <Card.Body>
+          <Card.Text>
+          <a href="https://wasim202.github.io/team-project-1/">Welcome to Movie Database</a>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src={HomePage} />
+        <Card.Body>
+          <Card.Text>
+          <a href="https://wasim202.github.io/quiz/">Welcome to Quiz Game</a>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src={ViewTables} />
+        <Card.Body>
+          <Card.Text>
+          <a href="https://github.com/wasim202/employee-tracker">Welcome to Tracking Employees System</a> 
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </Container>
+    </>
   )
 }
